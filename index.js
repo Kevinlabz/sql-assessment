26919d06699fb39bbaa756a6474c118ffb7c2b25
Kevin Labs <kevinlabs@outlook.com>
2017-04-20 17:30:56 -0600

    Unfinished issue

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var massive = require('massive');
-var port = 8888;
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const massive = require('massive');
+const port = 3000;
 
 //Need to enter username and password for your database
-var connString = "postgres://postgres:test@localhost/assessbox";
+const connString = "postgres://postgres:test@localhost/assessbox";
 
-var app = module.exports = express();
+const app = module.exports = express();
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -23,23 +23,25 @@ var db = massive.connect({
     db = localdb;
     app.set('db', db);
 
-    //Delete previous table
-    db.delete_users_table(function () {
-      console.log("Delete user table");
-    });
+    // //Delete previous table
+    // db.delete_users_table(function () {
+    //   console.log("Delete user table");
+    // });
 
-    db.delete_vehicles_table(function () {
-      console.log("Delete Vehicle table");
-    });
+    // db.delete_vehicles_table(function () {
+    //   console.log("Delete Vehicle table");
+    // });
+    // //========================
 
-    //Create new table.
-    db.create_users_table(function () {
-      console.log("Creating Users table");
-    });
+    // //Create new table.
+    // db.create_users_table(function () {
+    //   console.log("Creating Users table");
+    // });
 
-    db.create_vehicles_table(function () {
-      console.log("Creating vehicles table");
-    });
+    // db.create_vehicles_table(function () {
+    //   console.log("Creating vehicles table");
+    // });
+    // //========================
 
     //Inserting user  and vehicle data.
     db.user_create_seed(function () {
@@ -47,23 +49,67 @@ var db = massive.connect({
     });
 
     db.vehicle_create_seed(function () {
-      console.log("Vehicle Table Init")
+      console.log("Vehicle Table Init");
     });
   });
 
-var serverCtrl = require('./node_scripts/serverCtrl.js');
+const serverCtrl = require('./serverCtrl.js');
 
 
 
 //Requested Endpoints
-//app.get('/api/users',function () {console.log('test');}, serverCtrl.getAllUsers);
-//app.get('/api/vehicles', serverCtrl.getAllVehicles);
 
+//GET '/api/users'
+app.get('/api/users', function (req, res) {
+  console.log('Function getAllUsers Activated');
+  db.get_all_users(function (err, data) {
+    if (!err) {
+      res.status(200).send(data);
+    } else {
+      res.status(404).send('User not Found.', err);
+    }
+  });
+});
+
+//GET '/api/vehicles'
+app.get('/api/vehicles', function (req, res, next) {
+  console.log('Function getAllVehicles Activated');
+  db.getAllVehicles(function (err, data) {
+    if (!err) {
+      res.status(200).send(data);
+    } else {
+      res.status(404).send('User not Found.', err);
+    }
+  });
+});
+
+// //POST '/api/users'
+app.post('/api/users', function (req, res, next) {
+  console.log('Function add_users Activated');
+  console.log('Reqeust body data: ', req.body);
+  let firstName = req.body.firstname,
+    lastName = req.body.lastname,
+    email = req.body.email;
+
+  db.add_users([firstName, lastName, email], function (err, sqlResponse) {
+    if (!err) {
+      res.status(200).send(sqlResponse);
+    } else {
+      res.status(500).send('Error', err);
+    }
+  });
+});
+
+
+
+
+//===========================
 
 
 
 //Test Endpoints
 //app.get('/api/vehicles', serverCtrl.getAllVehicles);
+//==========================
 
 
 app.listen(port, function () {
