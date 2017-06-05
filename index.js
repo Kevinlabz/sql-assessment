2bd3295ddf5fd18cfc8228a18a33a75ab2f975d6
Kevin Labs <kevinlabs@outlook.com>
2017-06-05 14:55:51 -0600

    changes

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var massive = require('massive');
+
+
 //Need to enter username and password for your database
-var connString = "postgres://username:password@localhost/assessbox";
+const config = require('./config.js');
+//Connecting setting is inside the config file.
+
 
 var app = express();
 
@@ -13,18 +17,55 @@ app.use(cors());
 //The test doesn't like the Sync version of connecting,
 //  Here is a skeleton of the Async, in the callback is also
 //  a good place to call your database seeds.
-var db = massive.connect({connectionString : connString},
-  function(err, localdb){
+var db = massive.connect({
+    connectionString: config.connectionString
+  },
+  function (err, localdb) {
+    console.log('test showing err: ', err);
     db = localdb;
-    // app.set('db', db);
-    //
-    // db.user_create_seed(function(){
-    //   console.log("User Table Init");
+    app.set('db', db);
+
+    // //Delete previous table
+    // db.delete_users_table(function () {
+    //   console.log("Delete user table");
     // });
-    // db.vehicle_create_seed(function(){
-    //   console.log("Vehicle Table Init")
+
+    // db.delete_vehicles_table(function () {
+    //   console.log("Delete Vehicle table");
     // });
-})
+    // //========================
+
+    //Create new table.
+    db.create_users_table(function () {
+      console.log("Creating Users table");
+    });
+
+    db.create_vehicles_table(function () {
+      console.log("Creating vehicles table");
+    });
+    //========================
+
+    //Inserting user  and vehicle data.
+    db.user_create_seed(function () {
+      console.log("User Table Init");
+    });
+
+    db.vehicle_create_seed(function () {
+      console.log("Vehicle Table Init");
+    });
+  });
+
+app.get('/api/users', function (req, res) {
+  console.log('Function getAllUsers Activated');
+  db.get_all_users(function (err, data) {
+    if (!err) {
+      res.status(200).send(data);
+    } else {
+      res.status(404).send('User not Found.', err);
+    }
+  });
+});
+
 
 app.listen('3000', function(){
   console.log("Successfully listening on : 3000")
